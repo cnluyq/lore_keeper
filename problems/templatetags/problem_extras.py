f34e082234cebadf6e8858b69f47c5d10729ea1f
Andrew <yqmailpub@163.com>
2025-12-11 14:16:57 +0000

    refine view detail display

--- problems/templatetags/problem_extras.py
+++ problems/templatetags/problem_extras.py
@@ -6,15 +6,6 @@ from django import template
 register = template.Library()
 
 @register.filter
-def field_json(problem, field):
-    """
-    把 problem 的任意文本字段原样转义成 JSON，供 JS 直接嵌入。
-    用法：{{ problem|field_json:'description' }}
-    """
-    value = getattr(problem, field) or ''
-    return json.dumps(value)          # 自动转义引号、换行等
-
-@register.filter
 def basename(value):
     """
     去掉文件路径，只留文件名。
